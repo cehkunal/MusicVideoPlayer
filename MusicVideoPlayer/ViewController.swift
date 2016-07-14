58432d3ac121a0c7bc3eec47f17d898769b632be
Kunal Pachauri <kunal@kunals-mac.local>
2016-07-14 09:09:09 +0530

    Initial Commit

---
+++ MusicVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideoPlayer
+//
+//  Created by Kunal Pachauri on 7/14/16.
+//  Copyright © 2016 Kunal Pachauri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
